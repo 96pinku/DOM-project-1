4e52f23383609d565f65f209cc9c2a7965aeefa0
Pinku <pinkuparmer96@gmail.com>
2024-05-22 14:28:24 +0530

    DOM PROJECT 1

---
+++ script.js
@@ -0,0 +1,12 @@
+let divElement = document.getElementById("demo");
+function change(){
+    divElement.style.color = "blue";
+    divElement.style.backgroundColor = "yellow";
+    divElement.style.height= "400px";
+    divElement.style.width = "300px";
+    divElement.style.fontSize = "25px";
+    divElement.style.fontWeight = "bold";
+    divElement.style.margin = "30px";
+    divElement.style.padding = "15px";
+}
+change();
